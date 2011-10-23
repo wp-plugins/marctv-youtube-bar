ae44a4353b96bee153eef4f8b4f2d3e03923142e
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-23 20:59:21 +0000

    tagging 1.8
    git-svn-id: https://plugins.svn.wordpress.org/marctv-youtube-bar/tags/1.8@454808 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv-youtube-bar.php
+++ marctv-youtube-bar.php
@@ -1,11 +1,10 @@
 <?php
 /*
   Plugin Name: MarcTV Youtube Bar
-  Plugin URI: http://www.marctv.de/blog/2010/08/25/marctv-wordpress-plugins/
-  Description: Displays a list of videos from a youtube account. 
-  Version: 1.5
+  Plugin URI: http://www.marctv.de
+  Description:
+  Version: 1.8
   Author: Marc Tönsing
-
   Author URI: http://www.marctv.de
   License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
@@ -16,9 +15,8 @@
   your option) any later version.
  */
 
-
+$version = '1.8';
 function marctv_youtube_bar_scripts() {
-  $version = '1.5';
   wp_enqueue_style(
           "jquery.marctv-youtube-bar-style", WP_PLUGIN_URL . "/marctv-youtube-bar/marctv-youtube-bar.css",
           false, $version);
